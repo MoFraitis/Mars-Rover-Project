5d4d7c2100aa3fd1e8a640af6caa10bc49ec5573
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-14 14:33:07 +0000

    feat: implemented getters for PlateauSize

--- src/main/java/PlateauSize.java
+++ src/main/java/PlateauSize.java
@@ -1,3 +1,17 @@
 public class PlateauSize {
+    private final int x;
+    private final int y;
 
+    public PlateauSize(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
