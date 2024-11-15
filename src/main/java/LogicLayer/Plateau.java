f237b090678eae5aceb3e10d6ad63b32983f0939
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-15 15:32:00 +0000

    feat: added isWithinPlateau method that checks if a position is within the given plateau

--- src/main/java/LogicLayer/Plateau.java
+++ src/main/java/LogicLayer/Plateau.java
@@ -1,4 +1,15 @@
 package LogicLayer;
 
 public class Plateau {
+    private final PlateauSize plateauSize;
+
+    public Plateau(PlateauSize plateauSize) {
+        this.plateauSize = plateauSize;
+    }
+
+    public boolean isWithinPlateauSize(Position position) {
+        int x = position.getX();
+        int y = position.getY();
+        return x >= 0 && x <= plateauSize.getX() && y >= 0 && y <= plateauSize.getY();
+    }
 }
