26b058c32733a90bb65b8ff16d08418bcc5ce669
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-15 10:35:24 +0000

    feat: implemented parseInput method in PositionParser class

--- src/main/java/InputLayer/PositionParser.java
+++ src/main/java/InputLayer/PositionParser.java
@@ -1,4 +1,16 @@
 package InputLayer;
 
+import LogicLayer.CompassDirection;
+import LogicLayer.Position;
+
 public class PositionParser {
+
+    public static Position parseInput(String input) {
+        String[] positionArray = input.split("");
+        int x = Integer.parseInt(positionArray[0]);
+        int y = Integer.parseInt(positionArray[1]);
+        CompassDirection facing = CompassDirection.valueOf(positionArray[3]);
+
+        return new Position(x, y, facing);
+    }
 }
