8320c7f91855760fee7f33a250addfb950073e78
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-15 10:34:53 +0000

    feat: implemented parseInput method in PlateauSizeParser class with exception handling

--- src/main/java/InputLayer/PlateauSizeParser.java
+++ src/main/java/InputLayer/PlateauSizeParser.java
@@ -1,6 +1,23 @@
 package InputLayer;
 
-public class PlateauSizeParser {
-    
+import LogicLayer.PlateauSize;
 
+import java.util.InputMismatchException;
+
+public class PlateauSizeParser {
+
+    public static PlateauSize parseInput(String input) {
+        String inputWhiteSpacesRemoved = input.replaceAll(" ", "");
+
+        if (inputWhiteSpacesRemoved.matches("\\d+")) {
+            String[] plateauSizeArray = inputWhiteSpacesRemoved.split("");
+            int x = Integer.parseInt(plateauSizeArray[0]);
+            int y = Integer.parseInt(plateauSizeArray[1]);
+
+            return new PlateauSize(x, y);
+        } else {
+            throw new InputMismatchException("Plateau Size Can Only Contain 2 Positive Numbers");
+        }
+
+    }
 }
