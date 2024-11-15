2fc169622a3e4512cbf139190ae0c14019008f31
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-15 11:58:15 +0000

    feat: implemented exception handling on parseInput method in PositionParser

--- src/main/java/InputLayer/PositionParser.java
+++ src/main/java/InputLayer/PositionParser.java
@@ -3,14 +3,22 @@ package InputLayer;
 import LogicLayer.CompassDirection;
 import LogicLayer.Position;
 
+import java.util.InputMismatchException;
+
 public class PositionParser {
 
     public static Position parseInput(String input) {
-        String[] positionArray = input.split("");
-        int x = Integer.parseInt(positionArray[0]);
-        int y = Integer.parseInt(positionArray[1]);
-        CompassDirection facing = CompassDirection.valueOf(positionArray[3]);
+        String inputWhiteSpacesRemoved = input.replaceAll(" ", "").toUpperCase();
 
-        return new Position(x, y, facing);
+        if(inputWhiteSpacesRemoved.matches("^\\d{2}[NESW]$") && inputWhiteSpacesRemoved.length() == 3){
+            String[] positionArray = inputWhiteSpacesRemoved.split("");
+            int x = Integer.parseInt(positionArray[0]);
+            int y = Integer.parseInt(positionArray[1]);
+            CompassDirection facing = CompassDirection.valueOf(positionArray[2]);
+
+            return new Position(x, y, facing);
+        } else {
+            throw new InputMismatchException("Position must only contain an x and y co-ordinate and a compass direction (N,E,S or W)");
+        }
     }
 }
