4f034fa3cdfcb8ae6e34c4d5270ecc39b5d499bd
Mohamed Fraitis <mfraitis1@gmail.com>
2024-11-15 10:42:24 +0000

    feat: implemented parseInput method in InstructionParser class

--- src/main/java/InputLayer/InstructionParser.java
+++ src/main/java/InputLayer/InstructionParser.java
@@ -1,4 +1,19 @@
 package InputLayer;
 
+import LogicLayer.Instruction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstructionParser {
+
+    public static List<Instruction> parseInput(String input) {
+        List<Instruction> instructions = new ArrayList<>();
+
+        for (char c : input.toCharArray()) {
+            instructions.add(Instruction.valueOf(String.valueOf(c)));
+        }
+
+        return instructions;
+    }
 }
